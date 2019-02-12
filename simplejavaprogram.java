f92ac59b16e76ad9cf8b7d45d54609ec323ffe72
gavaha <44081811+gavaha@users.noreply.github.com>
2019-02-12 19:14:04 +0530

    Update simplejavaprogram.java

--- simplejavaprogram.java
+++ simplejavaprogram.java
@@ -2,7 +2,7 @@ public class Prime {
 
     public static void main(String[] args) {
 
-        int num = 29;
+        int num = 30;
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
